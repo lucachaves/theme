f4c03dc0c43d4a1326da79b9f498c59d42aeef38
Luiz Carlos <lucachaves@gmail.com>
2022-04-07 23:01:59 -0300

    remove logo of NabBar

--- src/vitepress/components/VPNavBarTitle.vue
+++ src/vitepress/components/VPNavBarTitle.vue
@@ -1,16 +1,12 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+const { site } = useData()
+</script>
+
 <template>
   <a class="VPNavBarTitle" href="/">
-    <svg class="logo" viewBox="0 0 128 128" width="24" height="24">
-      <path
-        fill="#42b883"
-        d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z"
-      />
-      <path
-        fill="#35495e"
-        d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z"
-      />
-    </svg>
-    <span class="text">Vue.js</span>
+    <span class="text" v-text="site.title"/>
   </a>
 </template>
 
@@ -25,14 +21,6 @@
 
 .VPNavBarTitle:hover {
   opacity: 0.6;
-}
-
-.logo {
-  position: relative;
-}
-
-.logo + .text {
-  padding-left: 8px;
 }
 
 .text {
